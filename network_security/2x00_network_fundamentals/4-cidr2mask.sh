c0c364d7b918c67b5a746adbd0fa05ca908aac94
FeridAbv <feridabv6577@gmail.com>
2026-05-30 12:59:44 +0000

    ta

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in 24 16 8 0; do printf "%d." $(( $1>i?255:$1<=i-8?0:256-(1<<(8-($1-i+8))) )); done | sed 's/\.$//'; echo
+for i in 24 16 8 0; do n=$(( $1-i )); printf "%d." $((n>=8?255:n<=0?0:256-(1<<(8-n)))); done | sed 's/\.$//'; echo
