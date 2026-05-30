a0a4e72376a5ac6664ca751c9fd4bfb4638cd228
FeridAbv <feridabv6577@gmail.com>
2026-05-30 13:08:35 +0000

    sd

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-cidr=$1
-
-for i in 1 2 3 4
-do
-    if [ $cidr -ge 8 ]; then
-        octet=255
-        cidr=$((cidr-8))
-    else
-        octet=$((256-2**(8-cidr)))
-        [ $cidr -eq 0 ] && octet=0
-        cidr=0
-    fi
-
-    printf "%d" "$octet"
-    [ $i -lt 4 ] && printf "."
-done
-
-echo
+echo "$((256-2**(32-$1)>>24&255)).$((256-2**(32-$1)>>16&255)).$((256-2**(32-$1)>>8&255)).$((256-2**(32-$1)&255))"
